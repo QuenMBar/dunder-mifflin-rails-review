12a8760a07a997ddd27378c57d089661cbbe4722
QuenMBar <Qbeast360@gmail.com>
2021-04-28 15:53:35 -0700

    DONE

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,9 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def to_s
+        emp_str = []
+        self.employees.each { |e| emp_str << e.full_name }
+        "name: #{name}, breed: #{breed}, age: #{age}, Employees: #{emp_str.join(', ')}"
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,14 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def to_s
+        "Name: #{self.full_name}, Alias: #{self.alias}, Title: #{self.title}, Office: #{self.office}, Dog: #{self.dog.name}"
+    end
 end
